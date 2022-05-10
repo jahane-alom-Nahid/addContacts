877fe73eb68584be089ae3e55296f67eb66ad091
jahane alom Nahid <61409420+jahane-alom-Nahid@users.noreply.github.com>
2022-05-10 11:46:46 +0600

    Add files via upload
    add contact from own device in android mobile, using java.

---
+++ caontactmodel.java
@@ -0,0 +1,26 @@
+package com.example.add_contactapp;
+
+public class caontactmodel {
+    String name,number;
+
+    public caontactmodel() {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
